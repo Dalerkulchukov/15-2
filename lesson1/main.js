20b2e4440691625a3fffd8d1d0a7e6dd3bbdcb06
DalerKulchukov <laxusdreyar20020709@gmail.com>
2022-06-04 21:33:57 +0600

    lesson1

---
+++ lesson1/main.js
@@ -0,0 +1,15 @@
+const a ="a";
+
+let c=32;
+
+
+const obj ={
+    key: "value",
+    username: "aybek",
+    data: {
+        first_name:"jack",
+        last_name: "rus"
+    }
+}
+const arr2 =[]
+
