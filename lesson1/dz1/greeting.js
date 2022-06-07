1b9cc93764844502216222fc2e3be5308ca0e9e7
DalerKulchukov <laxusdreyar20020709@gmail.com>
2022-06-08 01:07:03 +0600

    dz

---
+++ lesson1/dz1/greeting.js
@@ -0,0 +1,35 @@
+const name= prompt("Как вас зовут?")
+console.log("Hellow "+ name)
+
+const length1 = "Hellow what is your name?"
+console.log(length1.length)
+
+const  length2="hellow my friend , how are you?"
+console.log(length2.length)
+
+if(length1>length2)
+    console.log("1я больше")
+else if(length1<length2)
+    console.log("2я больше")
+else
+    console.log("они равны")
+
+let color= "red"
+
+switch (color){
+    case "red":
+        console.log("Color is red");
+        break;
+    case "yellow":
+         console.log("Color is yellow");
+        break;
+    case "green":
+         console.log("Color is green");
+        break;
+}
+ const arr3=[[[[[[[[[[[[[[[5]]]]]]]]]]]]]]]
+console.log(arr3[0])
+
+
+
+
